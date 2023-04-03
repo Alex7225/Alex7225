375494bd8db45ee5060d3718a0e3f07debeca00d
oleksandr <oleksandr_grigor@ukr.net>
2023-04-03 14:36:41 -0700

    first commit + next commit

---
+++ src/Print.java
@@ -0,0 +1,12 @@
+public class Print {
+
+    public int v =25;
+
+    public void printValue(){
+        String newValue = String.format("---<Return value = "+ v + ">---");
+        System.out.println(newValue);
+
+    }
+
+
+}
